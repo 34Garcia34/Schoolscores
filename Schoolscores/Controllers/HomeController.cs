c0e848ebb7f7fccb6239ab8160395402b282e61d
gerardo garcia <y5yy55y@outlook.com>
2021-12-17 20:36:40 -0600

    Addd HomeVM

--- Schoolscores/Controllers/HomeController.cs
+++ Schoolscores/Controllers/HomeController.cs
@@ -1,11 +1,13 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Schoolscores.Models;
+using Schoolscores.Models.ViewModels;
 using System.Diagnostics;
 
 namespace Schoolscores.Controllers
 {
     public class HomeController : Controller
     {
+
         private readonly ILogger<HomeController> _logger;
 
         public HomeController(ILogger<HomeController> logger)
@@ -15,7 +17,13 @@ namespace Schoolscores.Controllers
 
         public IActionResult Index()
         {
-            return View();
+            var vm = new HomeVM
+            {
+                Students = new List<Student>(),
+                Teachers = new List<Teacher>(),
+                ExamScores = new List<Examscores>()
+            };
+            return View(vm);
         }
 
         public IActionResult Privacy()
---
+++ Schoolscores/Models/ViewModels/HomeVM.cs
@@ -0,0 +1,9 @@
+﻿namespace Schoolscores.Models.ViewModels
+{
+    public class HomeVM
+    {
+        public List<Student> Students { get; set; }
+        public List<Teacher> Teachers { get; set; }
+        public List<Examscores> ExamScores { get; set; }
+    }
+}
