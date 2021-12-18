47a8499dcaa31268082f77ac36ec2719c598f323
jhood44 <jhood4@gmail.com>
2021-12-17 22:14:04 -0500

    Basic teacher controller. needs to be expanded on

--- Schoolscores/Controllers/TeacherController.cs
+++ Schoolscores/Controllers/TeacherController.cs
@@ -1,23 +1,17 @@
-﻿using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
-using CustomerInvoiceApp.Data;
-using CustomerInvoiceApp.Models;
-using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Mvc;
+using Schoolscores.Data;
+using Schoolscores.Models;
 
 namespace Schoolscores.Controllers
 {
     public class TeacherController : Controller
     {
-
         private readonly AppDbContext _context;
-
         public TeacherController(AppDbContext context)
         {
             _context = context;
         }
-
-        // GET: TeacherController
-        public ActionResult Index()
+        public IActionResult Index()
         {
             return View();
         }
@@ -28,90 +22,19 @@ namespace Schoolscores.Controllers
             return View();
         }
 
-        // GET: TeacherController/Create
         public ActionResult Create()
         {
-            Teachers teacher = new Teachers();
+            Teacher teacher = new Teacher();
             return View(teacher);
         }
 
         [HttpPost]
-        public IActionResult Create(Teachers teacher)
+        public IActionResult Create(Teacher teacher)
         {
             _context.Teachers.Add(teacher);
             _context.SaveChanges();
 
             return RedirectToAction("Index", "Home");
-        }
-
-        // POST: TeacherController/Create
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Create(IFormCollection collection)
-        {
-            try
-            {
-                return RedirectToAction(nameof(Index));
-            }
-            catch
-            {
-                return View();
-            }
-        }
-
-        // GET: TeacherController/Edit/5
-        public ActionResult Edit(int id)
-        {
-            Teachers teacher = _context.Teachers.FirstOrDefault(x => x.TeacherId == id);
-            return View(teacher);
-        }
-
-        [HttpPost]
-        public IActionResult Edit(Teachers teacher)
-        {
-            _context.Teachers.Update(teacher);
-            _context.SaveChanges();
-
-            return RedirectToAction("Index", "Home");
-        }
-
-        // POST: TeacherController/Edit/5
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Edit(int id, IFormCollection collection)
-        {
-            try
-            {
-                return RedirectToAction(nameof(Index));
-            }
-            catch
-            {
-                return View();
-            }
-        }
-
-        public IActionResult Delete(string id)
-        {
-            var teacher = _context.Teachers.FirstOrDefault(x => x.TeacherId == id);
-            _context.Teachers.Remove(teacher);
-            _context.SaveChanges();
-
-            return RedirectToAction("Index", "Home");
-        }
-
-        // POST: TeacherController/Delete/5
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Delete(int id, IFormCollection collection)
-        {
-            try
-            {
-                return RedirectToAction(nameof(Index));
-            }
-            catch
-            {
-                return View();
-            }
         }
     }
 }
---
+++ Schoolscores/Controllers/TeachersController.cs
@@ -0,0 +1,118 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Schoolscores.Models;
+using Schoolscores.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace Schoolscores.Controllers
+{
+    public class TeacherController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        private readonly AppDbContext _context;
+
+        public TeacherController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: TeacherController
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: TeacherController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: TeacherController/Create
+        public ActionResult Create()
+        {
+            Teachers teacher = new Teachers();
+            return View(teacher);
+        }
+
+        [HttpPost]
+        public IActionResult Create(Teachers teacher)
+        {
+            _context.Teachers.Add(teacher);
+            _context.SaveChanges();
+
+            return RedirectToAction("Index", "Home");
+        }
+
+        // POST: TeacherController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: TeacherController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            Teachers teacher = _context.Teachers.FirstOrDefault(x => x.TeacherId == id);
+            return View(teacher);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Teachers teacher)
+        {
+            _context.Teachers.Update(teacher);
+            _context.SaveChanges();
+
+            return RedirectToAction("Index", "Home");
+        }
+
+        // POST: TeacherController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        public IActionResult Delete(string id)
+        {
+            var teacher = _context.Teachers.FirstOrDefault(x => x.TeacherId == id);
+            _context.Teachers.Remove(teacher);
+            _context.SaveChanges();
+
+            return RedirectToAction("Index", "Home");
+        }
+
+        // POST: TeacherController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
