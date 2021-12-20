4712c0246b4efa6c724ed20b6072ca36d3c257b0
gerardo garcia <y5yy55y@outlook.com>
2021-12-19 22:47:33 -0600

    Builds and Starts table does not show tho

---
+++ Schoolscores/Migrations/20211220044309_ExamMigration.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Schoolscores.Migrations
+{
+    public partial class ExamMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ExamScore",
+                table: "Students");
+
+            migrationBuilder.CreateTable(
+                name: "ExamScores",
+                columns: table => new
+                {
+                    ExamId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    TearcherId = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    StudentId = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Testscore = table.Column<decimal>(type: "decimal(18,2)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ExamScores", x => x.ExamId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ExamScores");
+
+            migrationBuilder.AddColumn<string>(
+                name: "ExamScore",
+                table: "Students",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+    }
+}
